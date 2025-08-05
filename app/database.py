78319cf8755bdd44b31a0014071357ce29a73551
karenpaola.duran <karenpaola.duran.contractor@bbva.com>
2025-08-05 08:41:33 -0500

    add connection db

--- app/database.py
+++ app/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "postgresql://postgres:admin@localhost:5432/db_migration"
+DATABASE_URL = os.getenv("DATABASE_URL","postgresql://postgres:admin@localhost:5432/db_migration")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
